ab7cc75b02938d3b403e4f02cf1ebf6d39e968f3
ShomaHino <hino.pegasasu69.one@outlook.jp>
2023-07-15 16:21:51 +0900

    [カウンター]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -2,23 +2,36 @@ const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
 const minusButton = document.getElementById("minus-button")
 const multiplyButton = document.getElementById("multiply-button")
-
-let count = 0
+const distributeButton = document.getElementById("distribute-button")
+const number1 = document.getElementById("input-number1")
+const number2 = document.getElementById("input-number2")
+const AC = document.getElementById("AC")
+let outcome = 0
 
 // ボタン要素のonclickハンドラに関数を代入する
 plusButton.onclick = function () {
   // count を更新
-  count += 1
+  outcome = Number(number1.value) + Number(number2.value)
   // count を表示
-  display.textContent = count
+  display.textContent = outcome
 }
 
 minusButton.onclick = function () {
-  count -= 1
-  display.textContent = count
+  outcome = Number(number1.value) - Number(number2.value)
+  display.textContent = outcome
 }
 
 multiplyButton.onclick = function () {
-  count = count * 2
-  display.textContent = count
+  outcome = Number(number1.value) * Number(number2.value)
+  display.textContent = outcome
+}
+
+distributeButton.onclick = function () {
+  outcome = Number(number1.value) / Number(number2.value)
+  display.textContent = outcome
+}
+
+AC.onclick = function () {
+  outcome = 0
+  display.textContent = 0
 }
