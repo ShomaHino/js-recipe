06be072e0fb3c65b834af7bfd61f4654f84294ee
ShomaHino <hino.pegasasu69.one@outlook.jp>
2023-07-15 11:53:21 +0900

    [カウンター]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,5 +1,7 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const multiplyButton = document.getElementById("multiply-button")
 
 let count = 0
 
@@ -8,5 +10,15 @@ plusButton.onclick = function () {
   // count を更新
   count += 1
   // count を表示
+  display.textContent = count
+}
+
+minusButton.onclick = function () {
+  count -= 1
+  display.textContent = count
+}
+
+multiplyButton.onclick = function () {
+  count = count * 2
   display.textContent = count
 }
